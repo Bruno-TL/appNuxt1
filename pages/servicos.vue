1da7f31b59ab8c670fa6b9e6fe644612ef5b213d
Thiago <tbgt35@hotmail.com>
2023-05-03 08:42:07 -0300

    first commit

---
+++ pages/servicos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="bg-gray-100 ">
+        <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
+            <NuxtLink :to="{name: 'a-empresa'}">A empresa</NuxtLink>
+            <NuxtLink to="/servicos">Serviços</NuxtLink>
+        </div>
+    </div>
+    <div>
+        <div class="container  mx-auto">
+            <h1 class="text-lg font-bold">Serviços</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam beatae, doloribus minima quos consequatur dignissimos! Sequi enim in vitae nesciunt quidem ratione tempora, quisquam maxime. Sed illum ex incidunt!</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
