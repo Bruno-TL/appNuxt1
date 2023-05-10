4378dfa94a21bd31774656f1f5e31b8e61757b76
Thiago <tbgt35@hotmail.com>
2023-05-10 10:43:30 -0300

    asj

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'app3',
+    title: 'App-estudo',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'pt-br'
     },
     meta: [
       { charset: 'utf-8' },
